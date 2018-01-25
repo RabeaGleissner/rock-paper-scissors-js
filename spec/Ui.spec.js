7b20bf3cfb131101cf85a5c9f11635dd807a0e2b
Rabea Gleissner <rabeagleissner@gmail.com>
2018-01-25 09:11:41 +0000

    Completes Ui methods

--- spec/Ui.spec.js
+++ spec/Ui.spec.js
@@ -2,7 +2,7 @@ import Ui from '../src/Ui'
 
 describe('Ui', function() {
   let ui
-  let fakeConsole
+  let outputSpy
   let askedForInput = false
 
   function inputSpy() {
@@ -10,22 +10,37 @@ describe('Ui', function() {
   }
 
   beforeEach(() => {
-    fakeConsole = new FakeConsole()
-    ui = new Ui(inputSpy, fakeConsole)
+    outputSpy = new OutputSpy()
+    ui = new Ui(inputSpy, outputSpy)
+  })
+
+  it('greets the user', () => {
+    ui.greet()
+    expect(outputSpy.printedMessage()).toEqual('::: Welcome to Rock Paper Scissors :::')
   })
 
   it('asks user to enter a move', () => {
     ui.askForMove()
-    expect(fakeConsole.printedMessage()).toEqual('Enter your move:')
+    expect(outputSpy.printedMessage()).toEqual('Enter your move:')
   })
 
   it('gets user input', () => {
     ui.askForMove()
     expect(askedForInput).toBe(true)
   })
+
+  it('announces the winner', () => {
+    ui.announceWinner('you!')
+    expect(outputSpy.printedMessage()).toEqual('Winner: you!')
+  })
+
+  it('says goodbye', () => {
+    ui.sayBye()
+    expect(outputSpy.printedMessage()).toEqual('Byyyeeee!')
+  })
 })
 
-class FakeConsole {
+class OutputSpy{
   constructor() {
     this.message = ''
   }
--- src/Ui.js
+++ src/Ui.js
@@ -5,8 +5,25 @@ export default class Ui {
     this.output = output
   }
 
+  greet() {
+    this.output.log('::: Welcome to Rock Paper Scissors :::')
+  }
+
   askForMove() {
     this.output.log('Enter your move:')
     return this.input()
+  }
+
+  announceWinner(winner) {
+    this.output.log(`Winner: ${winner}`)
+  }
+
+  getReplayChoice() {
+    this.output.log('Do you fancy playing again?')
+    return this.input()
+  }
+
+  sayBye() {
+    this.output.log('Byyyeeee!')
   }
 }
